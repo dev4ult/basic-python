746c3cd2bd32304ec70c63e430a0de0d45021947
dev4ult <nibrasbiasaaja@gmail.com>
2023-02-14 21:39:54 +0700

    update collection

--- collections/dict.py
+++ collections/dict.py
@@ -1,10 +1,23 @@
 # ordered or unordered, changeable, and and no duplicates allowed
-# feels like json
 
 adict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964,
-  "year": 2020
 }
+print(adict)
+
+# accessing
+
+print("dict data : ", adict["brand"], adict["model"], adict["year"])
+
+print("keys : ", adict.keys()) # keys
+
+print("values : ", adict.values()) # values
+
+print("items : ", adict.items()) # items
+
+# new data 
+
+adict["seat"] = 4
 print(adict)
---
+++ collections/set.py
@@ -0,0 +1,11 @@
+# unordered, unchangeable, and do not allow duplicate values
+
+aset = {"apple", "banana", "orange"}
+print(aset)
+
+conset = set(("apple", "banana", "orange")) # constructor
+
+# accessing
+
+asetwithdupe = {"apple", "banana", "banana"}
+print(asetwithdupe)
