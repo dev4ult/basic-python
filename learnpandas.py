00a06bd4d5027e7b0fe51aaa74295c77339439f2
dev4ult <nibrasbiasaaja@gmail.com>
2023-03-05 13:00:59 +0700

    minor changes

--- learnpandas.py
+++ learnpandas.py
@@ -2,15 +2,6 @@ import pandas as pd
 
 print(pd.__version__)
 
-dataset = {
-    "names": ["erik", "siva", "ardi"],
-    "ages": [19, 20, 23],
-}
-
-asdataframe = pd.DataFrame(dataset)
-
-print(asdataframe)
-
 # pandas series
 
 arr_list = [1, 2, 3]
@@ -22,9 +13,37 @@ print("pandas series =")
 print(pandaseries)
 print("\n")
 
-# with index
+# set index
 
 pandaseriesetindex = pd.Series(arr_list, index=["a", "b", "c"])
 print("pandas series with index =")
 print(pandaseriesetindex)
 print("pandaseriesetindex['a'] = ", pandaseriesetindex["a"])
+
+# with dictionary
+
+person = {
+    "person1": "erik",
+    "person2": "ardri",
+    "person3": "vena",
+}
+
+pandaserieswithdict = pd.Series(person)
+print(pandaserieswithdict)
+
+# dataframe
+
+dataset = {
+    "names": ["erik", "siva", "ardi"],
+    "ages": [19, 20, 23],
+}
+
+asdataframe = pd.DataFrame(dataset)
+
+print(asdataframe)
+
+print("asdataframe.loc[0] : ")
+print(asdataframe.loc[0])
+
+print("asdataframe.loc[[0, 1]] : ")
+print(asdataframe.loc[[0, 1]])
